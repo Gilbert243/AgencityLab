ee63468bd6ca4934dd43cb9774104b0bb6b671c4
SomafGroup <support@somafgroup.com>
2026-08-12 21:33:04 +0200

    feat: export historical intensity references

--- agencitylab/extensions/__init__.py
+++ agencitylab/extensions/__init__.py
@@ -5,6 +5,13 @@ They do not promote experimental extension machinery into the canonical
 ``u -> beta -> b`` pipeline.
 """
 
+from .intensity_alternatives import (
+    INTENSITY_ALTERNATIVES_STATUS,
+    printed_offset_ratio_candidate,
+    raw_ratio_intensity,
+    sum_intensity,
+    sum_log_intensity,
+)
 from .riemannian import (
     RIEMANNIAN_EXTENSION_STATUS,
     riemannian_dynamic_intensity,
@@ -14,10 +21,15 @@ from .riemannian import (
 from .window_criteria import mean_contrast_criterion, orientational_entropy_criterion
 
 __all__ = [
+    "INTENSITY_ALTERNATIVES_STATUS",
     "RIEMANNIAN_EXTENSION_STATUS",
     "mean_contrast_criterion",
     "orientational_entropy_criterion",
+    "printed_offset_ratio_candidate",
+    "raw_ratio_intensity",
     "riemannian_dynamic_intensity",
     "riemannian_inner_product",
     "riemannian_speed",
+    "sum_intensity",
+    "sum_log_intensity",
 ]
